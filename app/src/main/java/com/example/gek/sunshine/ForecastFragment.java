101e3c6208fad210b80e747da6dc71350bc44749
Zhdanov Evgeniy <zhdanov.ek@gmail.com>
2016-10-26 23:19:17 +0300

    Work menu and AsyncTask

--- app/src/main/java/com/example/gek/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/gek/sunshine/ForecastFragment.java
@@ -1,9 +1,13 @@
 package com.example.gek.sunshine;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -11,6 +15,8 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -21,9 +27,33 @@ import java.util.ArrayList;
  */
 
 public class ForecastFragment extends Fragment {
-    private String TAG = "ForecastFragment: ";
 
     public ForecastFragment(){
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Указываем, что в этом фрагменте будет меню
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // заполняем меню с xml файла
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Проверяем наша ли кнопка нажата и обрабатываем
+        if (item.getItemId() == R.id.action_refresh) {
+//                Toast.makeText(getContext(), "click Button Refresh", Toast.LENGTH_SHORT).show();
+                FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+                fetchWeatherTask.execute(null, null, null);
+                return true;
+            }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -32,13 +62,14 @@ public class ForecastFragment extends Fragment {
         // false указывает на то, что не следует полученое вью вставлять в контейнер
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<>();
         list.add("Today Sunny - 88/63");
         list.add("Tomorrow - Foggy - 70/46");
         list.add("Weds - Cloudy - 72/63");
         list.add("Thurs - Rainy - 64/51");
         list.add("Fri - Foggy - 70-46");
         list.add("Sat - Sunny - 76-68");
+
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast,
                 R.id.list_item_forecast_textview, list);
@@ -47,42 +78,84 @@ public class ForecastFragment extends Fragment {
         ListView lvForecast = (ListView) rootView.findViewById(R.id.listview_forecast);
         lvForecast.setAdapter(adapter);
 
-        // Объявляем эти переменные вне try, что бы можно было их закрыт потом
-        HttpURLConnection urlConnection = null;
-        BufferedReader br = null;
-
-        // Сюда получаем ответ с сервера в формате json
-        String forecastJson = null;
-
-        try {
-            // TODO: 25.10.16 Вынести за рамки проекта ключ, что бы не светить на гитхабе
-            //      buildTypes.each {
-            //      it.buildConfigField 'String', 'OPEN_WEATHER_MAP_API_KEY', key123456superkey
-
-//                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=Cherkassy,ua&mode=json&cnt=7&lang=ru&units=metric&");
-            String baseUrl = "http://google.com.ua";
-            String apiKey = "&APPIID=";
-
-            URL url = new URL("http://google.com.ua");
-            urlConnection = (HttpURLConnection)url.openConnection();
-            urlConnection.connect();
-
-            String line = null;
-            br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            while (line == br.readLine()){
-                forecastJson += line;
-            }
-
-        } catch (Exception e){
-            e.printStackTrace();
-            Log.e(TAG,e.toString());
-        } finally {
-            Toast.makeText(getContext(), forecastJson, Toast.LENGTH_LONG).show();
-        }
-
         return rootView;
     }
 
-    //todo Создать асинтакс и перенести в него загрузку файла
+    /** AsyncTask для загрузки файла */
+    public class FetchWeatherTask extends AsyncTask<String, Integer, String>{
+        // Константу-таг для логов формируем по имени класса
+//        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+        private final String LOG_TAG = "MyLog";
+
+        @Override
+        protected String doInBackground(String... params) {
+            // Объявляем эти переменные вне try, что бы можно было их закрыт потом
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Сюда получаем ответ с сервера в формате json
+            String forecastJson = "";
+
+            try {
+                // в gradle.properties добавляем наш ключ, который хотим не бросать на гитхаб
+                // В build.gradle (app) добавляем строки инициализации этого ключа
+
+//            String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Cherkassy,ua&mode=json&cnt=7&lang=ru&units=metric&APPIID=";
+//            String apiKey = BuildConfig.MY_API_KEY;
+//            URL url = new URL(baseUrl.concat(apiKey));
+
+                //на время отладки юзаем другой урл
+                String myUrl = "" + BuildConfig.MY_API_KEY;
+
+                URL url = new URL(myUrl);
+                urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection.connect();
+
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+
+                forecastJson = buffer.toString();
+
+
+            } catch (IOException e){
+                e.printStackTrace();
+                Log.e(LOG_TAG, "IOException: ", e);
+                return null;
+            } finally {
+                // в конце закрываем соединение и поток если они открыты
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
 }
 
--- app/src/main/java/com/example/gek/sunshine/MainActivity.java
+++ app/src/main/java/com/example/gek/sunshine/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.gek.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 
