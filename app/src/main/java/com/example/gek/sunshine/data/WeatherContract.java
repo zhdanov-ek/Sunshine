193852d860449375279e4555bb79b094578bb9c0
Zhdanov Evgeniy <zhdanov.ek@gmail.com>
2016-11-17 23:22:09 +0200

    Work delete and update

--- app/src/main/java/com/example/gek/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/example/gek/sunshine/data/WeatherContract.java
@@ -80,6 +80,7 @@ public class WeatherContract {
 
         public static final String CONTENT_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
 
